fdb307d5cec0dd731867a9a4a275fe6f6619cbcb
timakin <timaki.st@gmail.com>
2014-02-05 01:10:36 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nyanda::Application.config.secret_key_base = '44a06d68f8e04ef344dfb73229d3c415956f627ff87162ed8ea267bd7185a3262f3a48df3799a745d2547fbda5f2e52e9f6e1436657dc720ad28cddca9227e68'
