e454162b9433e3f0534bb4e8ccfcb3025f721e66
timakin <timaki.st@gmail.com>
2014-02-08 15:54:08 +0900

    Gemfileやら

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 nyanda
 ======
 
-nyandaアプリのリポジトリ
+#参考にしたサイト
+
+・RailsにおけるHerokuの使い方まとめ
+http://exfreeter.hatenablog.com/entry/2013/12/09/235902
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Nyanda::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root :to => "home#index"
+  root :to => "topics#index"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
