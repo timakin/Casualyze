a1655e92e6b3ff8d85f4e21c2308e533cdb427d4
timakin <timaki.st@gmail.com>
2014-09-17 22:11:43 +0900

    nyanda to casualy

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Nyanda::Application.config.session_store :cookie_store, key: '_nyanda_session'
+Nyanda::Application.config.session_store :cookie_store, key: '_casualyze_session'
