024a712173b97952cd81bb98313479b7b6629af9
timakin <timaki.st@gmail.com>
2014-10-22 15:19:22 +0900

    remove wrong arguement

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -10,5 +10,5 @@ server '153.121.70.114', user: 'timakin', roles: %w{web app db}
 
 set :ssh_options, {
     keys: [File.expand_path('~/.ssh/id_rsa')],
-    forward_agent: true,
+    forward_agent: true
 }
