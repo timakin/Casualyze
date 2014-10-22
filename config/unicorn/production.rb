70515400f29e2a515d0a93fcfe189970de91a253
timakin <timaki.st@gmail.com>
2014-10-22 22:12:30 +0900

    fix

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -2,13 +2,13 @@ set :branch, 'master'
 #set :stage, :production  
 #set :rails_env, :production
 
-role :app, %w{timakin@153.121.70.114 }
+role :app, %w{timakin@153.121.70.114}
 role :web, %w{timakin@153.121.70.114}
 role :db,  %w{timakin@153.121.70.114}
 
 server '153.121.70.114', user: 'timakin', roles: %w{web app db}
 
 set :ssh_options, {
-    keys: '~/.ssh/id_rsa',
+    keys: %w('~/.ssh/id_rsa'),
     forward_agent: true
 }
