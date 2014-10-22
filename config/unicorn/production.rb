6ba76e21f3cbf736e6b0d23c38da43963e1bd98d
timakin <timaki.st@gmail.com>
2014-10-22 17:10:50 +0900

    ssh keys setting

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -9,6 +9,6 @@ role :db,  %w{timakin@153.121.70.114}
 server '153.121.70.114', user: 'timakin', roles: %w{web app db}
 
 set :ssh_options, {
-    keys: [File.expand_path('~/.ssh/id_rsa')],
+    keys: '~/.ssh/id_rsa',
     forward_agent: true
 }
