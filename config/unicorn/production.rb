04607e991668dbaac3b66f456a42c50fa23b67fa
timakin <timaki.st@gmail.com>
2014-10-23 14:14:26 +0900

    verbose debug

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -10,5 +10,6 @@ server '153.121.70.114', user: 'timakin', roles: %w{web app db}
 
 set :ssh_options, {
     keys: %w('~/.ssh/id_rsa'),
-    forward_agent: true
+    forward_agent: true,
+    verbose: :debug
 }
