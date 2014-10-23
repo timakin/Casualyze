da3ce362602f99bfab2148ad8abffdc8b58a0aea
timakin <timaki.st@gmail.com>
2014-10-23 19:39:59 +0900

    fix

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,6 +1,6 @@
-set :branch, 'master'
 #set :stage, :production  
 #set :rails_env, :production
+set :branch, 'master'
 
 role :app, %w{timakin@153.121.70.114}
 role :web, %w{timakin@153.121.70.114}
