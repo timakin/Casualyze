43a6053fef0e11b4ddbbf2fd417d192eeacb177b
timakin <timaki.st@gmail.com>
2014-10-24 21:14:13 +0900

    pubkey

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -9,7 +9,6 @@ role :db,  %w{timakin@153.121.70.114}
 server '153.121.70.114', user: 'timakin', roles: %w{web app db}
 
 set :ssh_options, {
-    keys: [File.expand_path('~/.ssh/id_rsa')],
-    forward_agent: true,
-    auth_methods: %w(publickey)
+    keys: [File.expand_path('~/.ssh/id_rsa.pub')],
+    forward_agent: true
 }
